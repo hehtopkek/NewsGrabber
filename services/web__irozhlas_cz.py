cd4c2642643b2e82b20b38b1ec4e88bc66f912e6
hehtopkek <49321875+hehtopkek@users.noreply.github.com>
2019-04-05 22:08:57 +0200

    Create web__irozhlas_cz.py

---
+++ services/web__irozhlas_cz.py
@@ -0,0 +1,8 @@
+refresh = 1800
+version = 20161106.01
+
+urls = ['http://www.irozhlas.cz/',
+        'https://www.irozhlas.cz/rss/irozhlas']
+regex = [r'^https?:\/\/[^\/]*irozhlas.cz\/']        
+videoregex = []
+liveregex = []
